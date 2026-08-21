0c4e74b39910f0017803be7017b3fb7f508ada73
Aditi-2403 <goeladiti24@gmail.com>
2026-08-21 17:34:24 +0530

    Initialize Terraform configuration for AWS S3 bucket

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "lab_bucket" {
+  bucket = "t-aws-lab-aditi-2026"
+
+  tags = {
+    Name = "T-AWS-Lab-Bucket"
+  }
+}
