2b2cc350cc876578bff7b08819dec365b60abb16
Aditi-2403 <goeladiti24@gmail.com>
2026-09-18 14:02:08 +0530

    Add S3 bucket policies for public access and HTTPS

--- main.tf
+++ main.tf
@@ -19,3 +19,36 @@ resource "aws_s3_bucket" "lab_bucket" {
     Experiment  = "Terraform-IaC"
   }
 }
+resource "aws_s3_bucket_public_access_block" "lab_bucket_public_access" {
+  bucket = aws_s3_bucket.lab_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "lab_bucket_https_only" {
+  bucket = aws_s3_bucket.lab_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "DenyInsecureTransport"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.lab_bucket.arn,
+          "${aws_s3_bucket.lab_bucket.arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      }
+    ]
+  })
+}
